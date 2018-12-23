25e963efea12e6ad64f52266c6b8500f62d15126
Spontier98 <samantha.pontier17@gmail.com>
2018-12-23 13:38:27 -0500

    Renamed files for LoRa boards

--- src/LoRa/Gateway/RFM95_Gateway.cpp
+++ src/LoRa/Gateway/RFM95_Gateway.cpp
@@ -1,27 +1,17 @@
-// Feather9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Feather9x_TX
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
-// for Feather32u4 RFM9x
+//Feather32u4
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 7
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
+#define LED 13
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
-
-// Blinky on receipt
-#define LED 13
 
 void setup()
 {
@@ -29,9 +19,7 @@ void setup()
     pinMode(RFM95_RST, OUTPUT);
     digitalWrite(RFM95_RST, HIGH);
 
-    Serial.begin(115200);
-
-    delay(100);
+    Serial.begin(9600);
 
     Serial.println("Feather LoRa RX Test!");
 
@@ -88,7 +76,7 @@ void loop()
             Serial.print("Freq Error: ");
             Serial.println(rf95.frequencyError(), DEC);
             // Send a reply
-            uint8_t data[] = "And hello back to you";
+            uint8_t data[] = "Acknowledged";
             rf95.send(data, sizeof(data));
             rf95.waitPacketSent();
             digitalWrite(LED, LOW);
