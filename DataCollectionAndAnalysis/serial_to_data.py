67387068e00767019143303f003904c83f6837a0
trnhx001 <39974811+trnhx001@users.noreply.github.com>
2020-07-13 17:03:20 -0400

    Optimized data collection with self-update for runtime

--- DataCollectionAndAnalysis/serial_to_data.py
+++ DataCollectionAndAnalysis/serial_to_data.py
@@ -2,54 +2,65 @@
 """
 This is a python script that handles and store message received by Gateway.
 """
-import sys, os, serial, signal, threading, time, json
+import sys, os, serial, signal, threading, time, json, math
 from datetime import datetime
 
 # callback functions
 def monitor(gateway_ser):
    data_point={'0':0,'1':0}
    point_set=set()
+   start = time.time()
    # loop to read incoming data unless terminated
    while (1):
         # read in incoming message in serial port
-        line = gateway_ser.readline()
-        line_str = str(line)
+        if(gateway_ser.in_waiting > 0):
+            line = gateway_ser.readline()
+            line_str = str(line)
+            print(line_str)
+            # Check if any data is read
+            if (line_str != ""):
+            #print line[:-1]         # strip \n
+                fields = line_str[2:-3] .split('/')
+                # Check if the data received is sensors data
+                if len(fields) > 1:
+                    start = time.time() 
+                    server_ID = fields[1]
+                    sensor_ID = fields[2]
+                    data_point[sensor_ID] = data_point[sensor_ID] + 1
+                    point_set.add(data_point[sensor_ID])
+                    data_point[sensor_ID] = len(point_set) 
+                    # generate string of data 
 
-        # Check if any data is read
-        if (line_str != ""):
-           #print line[:-1]         # strip \n
-            fields = line_str[2:-3] .split('/')
-            # Check if the data received is sensors data
-            if len(fields) > 1: 
-                server_ID = fields[1]
-                sensor_ID = fields[2]
-                data_point[sensor_ID] = data_point[sensor_ID] + 1
-                point_set.add(data_point[sensor_ID])
-                data_point[sensor_ID] = len(point_set) 
-                # generate string of data 
+                    # get current time at the time of receiving data
+                    now = datetime. now()
+                    cur_time = now. strftime("%D %H:%M:%S")
 
-                # get current time at the time of receiving data
-                now = datetime. now()
-                cur_time = now. strftime("%D %H:%M:%S")
+                    # get run number
+                    with open(os.getcwd() + "/run_config.json") as json_file:
+                        data = json.load(json_file)
+                        run_num = data['run_num']
+                    msg = "DataPoint " + str(data_point[sensor_ID]) + " Time "\
+                        + cur_time + " Run " + str(run_num) + " SensorID " + sensor_ID
+                    print("device ID: ", sensor_ID)
+                    text_file = open(os.getcwd() + "/gateway_data.txt", "a")
 
-                # get run number
-                with open(os.getcwd() + "/run_config.json") as json_file:
-                    data = json.load(json_file)
-                    run_num = data['run_num']
-                msg = "DataPoint " + str(data_point[sensor_ID]) + " Time "\
-                    + cur_time + " Run " + str(run_num) + " SensorID " + sensor_ID
-                print("device ID: ", sensor_ID)
-                text_file = open(os.getcwd() + "/gateway_data.txt", "a")
+                    # read in all measurements sent from the sensor
+                    for idx in range(3,len(fields),2):
+                        para = fields[idx]
+                        measure = fields[idx+1]     
+                        msg = msg + " " + para + " " + measure
 
-                # read in all measurements sent from the sensor
-                for idx in range(3,len(fields),2):
-                    para = fields[idx]
-                    measure = fields[idx+1]     
-                    msg = msg + " " + para + " " + measure
-
-                text_file.write(msg + "\n")
-                text_file.close()
-
+                    text_file.write(msg + "\n")
+                    text_file.close()
+        
+        else:
+            end = time.time()
+            duration = end - start
+            if duration >= 10.00:
+                print(duration)
+                print("Ending Program.....")
+                update_json()
+                sys.exit(0)
        # do some other things here
 
    print("Stop Monitoring")
